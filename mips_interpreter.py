5a7037f31a6f498792ae7f70047c8e9e3664c57d
Jonathan Gärtner <jonathan.gaertner@twooceans.de>
2023-03-14 19:13:46 +0100

    correcting actions, adding dependabot

--- mips_interpreter.py
+++ mips_interpreter.py
@@ -1,25 +1,25 @@
-import sys
+from sys import argv
 
 stack = [0]
-vars = {}
+variables = {}
 jump_points = {}
 regs = {"$sp": 0, "$v0":0}
 
 data = []
-with open("test.asm") as file:  # sys.argv[1]
-    current_line = 0
-    in_program = False
+with open(argv[1], encoding="UTF-8") as file:
+    CURRENT_LINE = 0
+    IN_PROGRAM = False
     for line in file:
         line = line[:-1]  # Removing newline character
         if line == ".text":
-            in_program = True  # Finished (last blank line)
-        elif not in_program:
+            IN_PROGRAM = True  # Finished (last blank line)
+        elif not IN_PROGRAM:
             continue
         elif line.endswith(":"):
-            jump_points[data[:-1]] = current_line
+            jump_points[data[:-1]] = CURRENT_LINE
         else:
             data.append(line)
-            current_line += 1
+            CURRENT_LINE += 1
 
 
 def regs_to_num(num):
@@ -28,18 +28,18 @@ def regs_to_num(num):
     return int(num)
 
 
-current_line = 0
-while current_line < len(data):
-    line = data[current_line].replace(",", "").split(" ")
+CURRENT_LINE = 0
+while CURRENT_LINE < len(data):
+    line = data[CURRENT_LINE].replace(",", "").split(" ")
     match line:
         case "li", reg, num:
             regs[reg] = int(num)
-        
+
         case "mul", erg, num1, num2:
             num1 = regs_to_num(num1)
             num2 = regs_to_num(num2)
             regs[erg] = regs[num1] * regs[num2]
-        
+
         case "add", erg, num1, num2:
             num1 = regs_to_num(num1)
             num2 = regs_to_num(num2)
@@ -47,42 +47,42 @@ while current_line < len(data):
                 if num1 + num2 >= len(stack):
                     stack.append(0)
             regs[erg] = num1 + num2
-        
+
         case "sw", reg, "($sp)":
             stack[regs["$sp"]] = regs[reg]
-        
+
         case "sw", reg, var:
             if "$sp" in var:
-                offset = int(var.split("(")[0]) / 4
+                OFFSET = int(var.split("(")[0]) / 4
                 stack[regs["$sp"] - 4] = regs[reg]
-            vars[var] = regs[reg]
-        
+            variables[var] = regs[reg]
+
         case "lw", reg, var:
             if "$sp" in var:
                 if not var.startswith("("):
-                    offset = int(var.split("(")[0]) / 4
+                    OFFSET = int(var.split("(")[0]) / 4
                 else:
-                    offset = 0
-                regs[reg] = stack[regs["$sp"] + offset]
+                    OFFSET = 0
+                regs[reg] = stack[regs["$sp"] + OFFSET]
             else:
-                regs[reg] = vars[var]
-        
+                regs[reg] = variables[var]
+
         case ["syscall"]:
             if regs["$v0"] == 1:
                 print(regs["$a0"])
             elif regs["$v0"] == 5:
                 regs["$v0"] = input()
-        
+
         case "bgt", num1, num2, label:
             num1 = regs_to_num(num1)
             num2 = regs_to_num(num2)
             if num1 > num2:
-                current_line = jump_points[label]
-        
+                CURRENT_LINE = jump_points[label]
+
         case "j", label:
-            current_line = jump_points[label]
+            CURRENT_LINE = jump_points[label]
 
         case _:
-            raise Exception("unkown instruction encountered: " + " ".join(line))
-    
-    current_line += 1
+            raise ValueError("unkown instruction encountered: " + " ".join(line))
+
+    CURRENT_LINE += 1
