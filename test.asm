c9141e98a0f4e1a0924cc888267d197230113d32
Jonathan Gärtner <jonathan.gaertner@twooceans.de>
2023-04-19 19:22:13 +0200

    everything going well not

--- test.asm
+++ test.asm
@@ -1,15 +1,113 @@
 .data
-y: .word 0
 x: .word 0
+y: .word 0
 z: .word 0
 .text
-li $t0, 4
+li $t0, 0
+add $sp, $sp -4
 sw $t0 ($sp)
+lw $t0 ($sp)
+add $sp, $sp, 4
+sw $t0, z
+li $t0, 1
+add $sp, $sp -4
+sw $t0 ($sp)
+li $t0, 2
+add $sp, $sp -4
+sw $t0 ($sp)
+lw $t0, ($sp)
+add $sp, $sp, 4
+lw $t1, ($sp)
+mul $t2, $t1, $t0
+sw $t2, ($sp)
+li $t0, 3
+add $sp, $sp -4
+sw $t0 ($sp)
+lw $t0, ($sp)
+add $sp, $sp, 4
+lw $t1, ($sp)
+add $t2, $t1, $t0
+sw $t2, ($sp)
+lw $t0 ($sp)
 add $sp, $sp, 4
 sw $t0, x
 li $v0, 5
 syscall
-li $t0, 5
-add $a0, $t0, $v0
+sw $v0, y
+while0start:
+li $t0, 0
+add $sp, $sp -4
+sw $t0 ($sp)
+lw $t0, x
+add $sp, $sp, -4
+sw $t0, ($sp)
+lw $t0 ($sp)
+lw $t1 4($sp)
+add $sp, $sp, 8
+bgt $t0, $t1, while0end
+lw $t0, x
+add $sp, $sp, -4
+sw $t0, ($sp)
+lw $t0, ($sp)
+mul $t1, $t0, -1
+sw $t0, ($sp)
+lw $t0, ($sp)
+add $sp, $sp, 4
+lw $t1, ($sp)
+add $t2, $t1, $t0
+sw $t2, ($sp)
+lw $t0 ($sp)
+add $sp, $sp, 4
+sw $t0, x
+lw $t0, z
+add $sp, $sp, -4
+sw $t0, ($sp)
+lw $t0, y
+add $sp, $sp, -4
+sw $t0, ($sp)
+lw $t0, ($sp)
+add $sp, $sp, 4
+lw $t1, ($sp)
+add $t2, $t1, $t0
+sw $t2, ($sp)
+lw $t0 ($sp)
+add $sp, $sp, 4
+sw $t0, z
+j while0start
+while0end:
+lw $t0, z
+add $sp, $sp, -4
+sw $t0, ($sp)
+li $t0, 1
+add $sp, $sp -4
+sw $t0 ($sp)
+lw $t0 ($sp)
+lw $t1 4($sp)
+add $sp, $sp, 8
+bgt $t0, $t1, if0end
+li $t0, 0
+add $sp, $sp -4
+sw $t0 ($sp)
 li $v0, 1
+lw $a0, ($sp)
 syscall
+add $sp, $sp, 4
+if0end:
+li $t0, 0
+add $sp, $sp -4
+sw $t0 ($sp)
+lw $t0, z
+add $sp, $sp, -4
+sw $t0, ($sp)
+lw $t0 ($sp)
+lw $t1 4($sp)
+add $sp, $sp, 8
+bgt $t0, $t1, if1end
+lw $t0, z
+add $sp, $sp, -4
+sw $t0, ($sp)
+li $v0, 1
+lw $a0, ($sp)
+syscall
+add $sp, $sp, 4
+if1end:
